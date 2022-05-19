f66a9e79385a72f43923e36154d31014dc933f22
mohyfahim <fahimohy@gmail.com>
2022-05-19 20:14:43 +0430

    comment sanity check

--- examples/get-started/main/main.c
+++ examples/get-started/main/main.c
@@ -12,11 +12,13 @@ static const char *TAG = "example";
 void app_main(void)
 {
     long unsigned int t = esp_timer_get_time();
-    uint8_t data[4] = {0b10101101, 0b01111010, 0b11010110, 0b10110101};     // 32 bit data
-    uint8_t err_data[4] = {0b10001101, 0b11111010, 0b11010110, 0b10110101}; // 2 - 8 err locations
+    uint8_t data[1] = {0b10101101}; //0b01111010, 0b11010110, 0b10110101};     // 32 bit data
+    uint8_t err_data[1] = {0b01001111}; //0b11111010, 0b11010110, 0b10110101}; // 2 - 8 err locations
     unsigned int errloc[40] = {0};
+
+    int ss = sizeof(data);
     struct bch_control *bch;
-    bch = init_bch(7, 15, 0);
+    bch = init_bch(7, 23, 0);
 
     ESP_LOGW(TAG, "size of ecc %d bytes", bch->ecc_bytes);
     ESP_LOGW(TAG, "size of ecc %d bits", bch->ecc_bits);
@@ -24,7 +26,7 @@ void app_main(void)
     memset(ecc, 0, bch->ecc_bytes);
     ESP_LOGW(TAG, "size of data = %d ", bch->n - bch->ecc_bits);
 
-    encode_bch(bch, data, 4, ecc);
+    encode_bch(bch, data, ss, ecc);
 
     for (int i = 0; i < bch->ecc_bytes; i++)
     {
@@ -32,31 +34,31 @@ void app_main(void)
     }
     printf("\n");
 
-    int err = decode_bch(bch, err_data, 4, ecc, NULL, NULL, errloc);
+    int err = decode_bch(bch, err_data, ss, ecc, NULL, NULL, errloc);
     ESP_LOGW(TAG, "decode err is = %d", err);
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < ss; i++)
     {
-        printf("%u", err_data[i]);
+        printf("%u ", err_data[i]);
     }
     printf("\n");
 
     for (int i = 0; i < 40; i++)
     {
         printf("%d ", errloc[i]);
-        if (errloc[i] != 0 && errloc[i] < 8 * 4)
+        if (errloc[i] != 0 && errloc[i] < 8 * ss)
         {
             err_data[errloc[i] / 8] ^= 1 << (errloc[i] % 8);
         }
     }
     printf("\n");
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < ss; i++)
     {
         printf("%u ", data[i]);
     }
     printf("\n");
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < ss; i++)
     {
         printf("%u ", err_data[i]);
     }
@@ -66,7 +68,7 @@ void app_main(void)
     unsigned char sha256[32];
     mbedtls_sha256_init(&sha256_ctx);
     mbedtls_sha256_starts(&sha256_ctx, false);
-    mbedtls_sha256_update(&sha256_ctx, err_data, 4);
+    mbedtls_sha256_update(&sha256_ctx, err_data, ss);
     mbedtls_sha256_finish(&sha256_ctx, sha256);
     for (int i = 0; i < 32; i++)
     {
